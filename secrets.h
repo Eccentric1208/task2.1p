44012c4938d3976f00a8ec43b9e30cee75a6f70e
Eccentric1208 <74599642+Eccentric1208@users.noreply.github.com>
2024-04-14 21:26:38 +1000

    secrets.h

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "TP-Link_729D"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "tplink123"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2482177		// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "9CDPMK1T5RNFF945"   // replace XYZ with your channel write API Key
